76622bc68505592b87f1b4134e37ba2facfc1669
cristian salazar <salazarcristiand@gmail.com>
2022-01-01 13:50:22 -0500

    actualisación de form y funcionalidad en js

--- js/main.js
+++ js/main.js
@@ -1,54 +1,48 @@
 //cuadrado
-function perimetroCu(lado){
+function perimetroCuadrado(lado){
     return lado * 4;
-    console.log("los lados del cuadrado miden: " + cuadrado + "cm")
-}
+  }
 
-function areaCu(lado){
+function areaCuadrado(lado){
     return lado * lado;
 }
 
-/*const cuadrado = 5;
-console.log("los lados del cuadrado miden: " + cuadrado + "cm");
-const perimetro = cuadrado * 4; 
-console.log("eL perímetro del cuadrado ");
-*/
-//triángulo
-  function perimTri(lado, base){
+// código triángulo 
+  function perimetroTriangulo(lado, base){
       return (lado + lado) + base
   }
-  function areaTr(altura, base){
+  function areaTriangulo(altura, base){
       return (altura * base) / 2; 
   }
 
 // codigo circulo
 const pi = Math.floor(Math.PI);
 
-  function diaCir(radio){
+  function diametroCirculo(radio){
     return radio * 2;
   }
-  function perimeCir(radio){
+  function perimetroCirculo(radio){
       const diametro = diaCir(radio);
       return diametro * pi;
   }
   
-  function areaCir(radio){   
+  function areaCirculo(radio){   
     return (radio * radio)* pi;
 }
 
-
-function calcularPerimetrocuadrado(){
+//
+function calcularPerimetroCuadrado(){
     const input = document.getElementById("inputCuadrado");
     const value = input.value;
 
-    const perimetroA = perimetroCu(value);
-    alert("perimetroA");
+    const perimetroA = perimetroCuadrado(value);
+    alert(perimetroA);
 
 }
-function calcularAreacuadrado(){
+function calcularAreaCuadrado(){
     const input = document.getElementById("inputCuadrado");
     const value = input.value;
 
-    const areas = areaCu(value);
-    alert("areas");
+    const areas = areaCuadrado(value);
+    alert(areas);
 }
